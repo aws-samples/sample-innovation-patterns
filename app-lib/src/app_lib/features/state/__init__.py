a3804d8c5d79d35425e43200b0a253849598763d
Vincil Bishop <vincilb@amazon.com>
2026-06-02 13:03:09 -0500

    Update: modify 1 file(s), delete 24 file(s)

--- app-lib/src/app_lib/features/state/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
-"""Singleton state management feature.
-
-Manages a single state record with a random banded level.
-"""
